5be941fb5a418d864946024513088f7683d4a86b
ilahajiyeva <44476352+ilahajiyeva@users.noreply.github.com>
2021-03-12 17:49:11 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,19 @@
-$(document).ready(function () {
-    $(window).scroll(function () {
-        if (this.scrollY > 20) {
-            $('.navbar').addClass("sticky");
-        } else {
-            $('.navbar').removeClass("sticky");
-        }
-        if (this.scroll > 500) {
-            $('.scroll-up-btn').addClass("show");
-        } else {
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
-
-
-//   slide-up script
-    $('.scroll-up-btn').click(function () {
-        $('html').animate({scrollTop: 0});
-    });
-
-//    typing animation script
-    var typed = new Typed(".typing", {
-        strings: ["Front-end Developer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-
-//   toggle menu / navbar script
-    $('.menu-btn').click(function () {
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
-//    owl carousel script
-    $(".carousel").owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0: {
-                items: 1,
-                nav: false
-            },
-            600: {
-                items: 2,
-                nav: false
-            },
-            1000: {
-                items: 3,
-                nav: false
-            }
-        }
-    });
+var typed = new Typed(".typing", {
+    strings: ["Front-end Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
 });
+
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "110px";
+    document.getElementById("main").style.marginLeft = "110px";
+    document.getElementById('main').style.opacity = "0";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.getElementById('main').style.opacity = "1";
+}
